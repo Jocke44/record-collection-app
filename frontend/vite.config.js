d3858ac7def581ede529151417a9014ccce4ccb3
Joakim Svan <87295906+Jocke44@users.noreply.github.com>
2025-05-06 21:13:18 +0200

    Ready for Render deplyment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,11 @@
 
+// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 
 export default defineConfig({
+  base: "./", // ✅ Add this for correct asset resolution on Render
   plugins: [react()],
   resolve: {
     alias: {
