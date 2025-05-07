2e2190f61673c98975e77173920a2247a18abd95
Joakim Svan <87295906+Jocke44@users.noreply.github.com>
2025-05-07 21:44:56 +0200

    upadet main.py

--- backend/main.py
+++ backend/main.py
@@ -22,7 +22,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:5173"
+        "http://localhost:5173",
          "https://record-frontend.onrender.com"          
     ],
     allow_methods=["*"],
